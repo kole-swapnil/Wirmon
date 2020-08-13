02a90bcad02774dce45a2c621270deb91d7007c2
sudhanshu1993 <49163737+sudhanshu1993@users.noreply.github.com>
2020-08-13 18:10:47 +0530

    Update digital_marketing.php
    add script

--- digital_marketing.php
+++ digital_marketing.php
@@ -80,6 +80,10 @@ if(isset($_POST['submit']))
     <title>Wirmon &mdash; IT Services</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 
     <link rel="stylesheet" href="css/custom-bs.css">
