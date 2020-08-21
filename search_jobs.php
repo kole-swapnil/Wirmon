198650fb4bc8d2e068dfc39afbd54f3f098bd10a
kole-swapnil <swapnilkole7500@gmail.com>
2020-08-21 20:07:50 +0530

    Search jobs added

---
+++ search_jobs.php
@@ -0,0 +1,269 @@
+<?php
+ session_start();
+ if (($_SESSION['email'] == '') || (!isset($_SESSION['email']))) {
+      header("Location: login.php");
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Wirmon &mdash; Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="css/custom-bs.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="fonts/line-icons/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/quill.snow.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/dash.css">
+<style>@media only screen and (max-width: 521px){
+  .ml-auto{display:none;}
+  .icon-menu{margin-right: -120px;}
+  .logout{display: block !important;}
+}</style>
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body id="top">
+
+
+<div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+
+
+    <!-- NAVBAR -->
+    <header class="site-navbar mt-3">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="site-logo col-6"><a href="index.php">Wirmon</a></div>
+
+          <nav class="mx-auto site-navigation">
+            <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
+              <li><a href="index.php" class="nav-link">Home</a></li>
+              <li><a href="about.php">About</a></li>
+              <li><a href="job-listings.php">Jobs</a> </li>
+                <li class="has-children">
+                  <a>Services</a>
+                  <ul class="dropdown">
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="portfolio.php">Portfolio</a></li>
+                    <li><a href="faq.php">Frequently Ask Questions</a></li>
+                  </ul>
+                </li>
+              <li><a href="contact.php">Contact</a></li>
+              <li class="logout" style="display:none"><a href="logout.php"><i class="icon-sign-out" style="padding-left:5%;"></i>Logout</a></li>
+
+              </ul>
+            </nav>
+
+            <div class="right-cta-menu text-right d-flex aligin-items-center col-6">
+              <div class="ml-auto">
+                <div class="dropdown"><span class="mr-2 icon-lock_outline dropdown-toggle" data-toggle="dropdown" style="color:#fff;">
+                    <?php echo $_SESSION['email']; ?></span>
+    <ul class="dropdown-menu">
+      <li><a href="logout.php"><i class="icon-sign-out" style="padding-left:5%;"></i>Logout</a></li>
+
+    </ul>
+  </div>
+  </div>
+            <a href="#" class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span class="icon-menu" id = "navicon" style="height:130px;width:130px;"></span></a>
+          </div>
+
+        </div>
+      </div>
+    </header>
+    <!-- HOME -->
+    <section class="section-hero overlay inner-page bg-image" style="background-image: url('images/hero_11.jpg');padding-bottom:unset;" id="home-section">
+    </section>
+
+    <section class="site-section">
+      <div class="content-container">
+<div class="row">
+  <div class="col-md-3 LeftNavSideBar" valign="top">
+  <div class="col-md-12 " valign="top">
+    <!-- Sidebar -->
+    <div class="navbar navbar-default" role="navigation">
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle pull-left" data-toggle="collapse" data-target=".navbar-employerLeftNav-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand navbar-right text-ncspBlue paddingLeft0"><i class="icon-arrow-left" style="padding-right:3%;"></i>Employer Menu</a>
+        </div>
+        <div id="sidebar-wrapper" class="collapse navbar-collapse navbar-employerLeftNav-collapse paddingLeftRight0" style="padding-left:unset;">
+            <nav id="spy">
+                <ul class="sidebar-nav nav" style="display:block !important;">
+                      <li class="hidden-md hidden-lg">
+                        <a  href="index.php">Home</a>
+                    </li>
+                    <li class="active">
+                        <a href="">Dashboard</a></li>
+                    <li class="enabled">
+                        <a href="js_profile.php">View/Update Profile</a>
+                    </li>
+                  <li class="enabled">
+                        <a href="">Search Jobs</a>
+                    </li>
+                    <li class="enabled">
+                        <a href="">Jobs Applied</a>
+                    </li>
+                              </ul>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+
+<div class="col-md-6 LeftNavSideBar">
+
+  <input style =" 
+  font-size: 17px;
+  border: 1px solid grey;
+  float: left;
+
+  background: #f1f1f1;"class="form-control" type="text" placeholder="Search" name="search" aria-label="Search">
+ 
+
+<br/>
+
+                 
+                </div>
+
+                <div class="column" id = "le" style = "flex: 25%;max-width: 25%;padding: 0 4px;font-weight:normal;">
+
+<div id="filtersection" style="display:block;padding: 10px 20px 20px 20px;
+background-color: #216945;
+border: 1px solid #ddd;
+border-radius: 4px;
+padding-right: auto;
+margin-right:20px;
+font-weight:normal !important;">
+    <h4 style = "background-color:#a7e7ff;padding:10px;">Search by</h4><hr>
+    <div class="list-group">
+           <h5 style="font-weight:bold;color:#fff;">Title</h5>
+           <div class = "col-12"style="padding-left: 0;padding-right: 0;">
+
+           <div class="list-group-item ">
+               <label><input type="checkbox" class="common_selector type" value="Hello" > Hii</label>
+           </div>
+          
+       </div>
+       </div>
+<div class="list-group">
+           <h5 style="font-weight:bold;color:#fff;">Location</h5>
+           <div class = "col-12"style="padding-left: 0;padding-right: 0;">
+
+           <div class="list-group-item ">
+               <label><input type="checkbox" class="common_selector loc" value="Hello"  > Hii</label>
+           </div>
+           
+           </div>
+       </div>
+
+       <div class="list-group">
+           <h5 style="font-weight:bold;color:#fff;">Experience</h5>
+               <div class = "col-12"style="padding-left: 0;padding-right: 0;">
+
+           <div class="list-group-item ">
+               <label><input type="checkbox" class="common_selector exp" value="Hello"  > Hii</label>
+           </div>
+           
+       </div>
+       </div>
+
+       <div class="list-group">
+                         <h5 style="font-weight:bold;color:#fff;">Salary</h5>
+                         <div class = "col-12"style="padding-left: 0;padding-right: 0;">
+                        
+                         <div class="list-group-item ">
+                             <label><input type="checkbox" class="common_selector sal" value="Hello"  > Hii</label>
+                         </div>
+                        
+                         </div>
+                     </div>
+                     <div class="list-group">
+                             <h5 style="font-weight:bold;color:#fff;">Skills</h5>
+                                       <div class = "col-12"style="padding-left: 0;padding-right: 0;">
+                            
+                                       <div class="list-group-item ">
+                                           <label><input type="checkbox" class="common_selector skills" value="Hello"  > Hii</label>
+                                       </div>
+                                       
+                                       </div>
+                                   </div>
+                                   <div class="list-group">
+                                                     <h5 style="font-weight:bold;color:#fff;">Education</h5>
+                                                     <div class = "col-12"style="padding-left: 0;padding-right: 0;">
+                                                    
+                                                     <div class="list-group-item ">
+                                                         <label><input type="checkbox" class="common_selector edu" value="Hello"> Hii</label>
+                                                     </div>
+                                                    
+                                                     </div>
+                                                 </div>
+       <p id="clearfilter" style="font-weight:bold; color:red;cursor: pointer;"></p>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+
+
+</div>
+
+
+    </section>
+    <div class="row align-items-center row-content" >
+
+
+</div>
+
+    <?php include_once 'footer.php'; ?>
+  </div>
+
+    <!-- SCRIPTS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/stickyfill.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/quill.min.js"></script>
+
+
+    <script src="js/bootstrap-select.min.js"></script>
+
+    <script src="js/custom.js"></script>
+
+
+
+  </body>
+</html>
