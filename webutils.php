cc4a4c707cfec8935169afb6d7545c8e8ab8dc07
soundarya-karli <soundaryakarli@gmail.com>
2020-08-21 15:45:36 +0530

    email password changed

--- webutils.php
+++ webutils.php
@@ -38,7 +38,7 @@ class WebUtils
         $mail->SMTPSecure = 'ssl';
         $mail->SMTPAuth = true;
         $mail->Username = "noreply@wirmon.in";
-        $mail->Password = "Wirmon@tech#567";
+        $mail->Password = "123@Response#90";
 
         $mail->From = "noreply@wirmon.in";
         $mail->FromName = "Wirmon India Pvt Ltd";
@@ -103,7 +103,7 @@ public function userMailToEmployer($mail, $userEmail,$id)
       $mail->SMTPSecure = 'ssl';
       $mail->SMTPAuth = true;
       $mail->Username = "noreply@wirmon.in";
-      $mail->Password = "Wirmon@tech#567";
+      $mail->Password = "123@Response#90";
 
       $mail->From = "noreply@wirmon.in";
       $mail->FromName = "Wirmon India Pvt Ltd";
@@ -144,7 +144,7 @@ public function userContactForm($mail, $userName, $userEmail)
         $mail->SMTPSecure = 'ssl';
         $mail->SMTPAuth = true;
         $mail->Username = "noreply@wirmon.in";
-        $mail->Password = "Wirmon@tech#567";
+        $mail->Password = "123@Response#90";
 
         $mail->From = "noreply@wirmon.in";
         $mail->FromName = "Wirmon India Pvt Ltd";
@@ -185,7 +185,7 @@ public function adminContactForm($mail, $userName, $mobile, $email, $subject, $m
         $mail->SMTPSecure = 'ssl';
         $mail->SMTPAuth = true;
         $mail->Username = "noreply@wirmon.in";
-        $mail->Password = "Wirmon@tech#567";
+        $mail->Password = "123@Response#90";
 
         $mail->From = "noreply@wirmon.in";
         $mail->FromName = "Wirmon India Pvt Ltd";
