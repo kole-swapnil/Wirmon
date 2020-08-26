f70367a245cd71e96c2e81f419296525571689c0
kole-swapnil <swapnilkole7500@gmail.com>
2020-08-26 22:19:38 +0530

    Title fixed for outer pages

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $total_pages = ceil($total_rows / $no_of_records_per_page);
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Wirmon</title>
+    <title>Welcome to Wirmon</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Job Portal for jobseeker and employer" />
