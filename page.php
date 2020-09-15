1538fd94060c668ed7f5ac857c4039febf9741ce
msasan25 <manpreetsasan252gmail.com>
2020-09-15 14:14:30 +0530

    page

---
+++ page.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Welcome to Wirmon</title>
+    <meta charset="utf-8">
+
+    <?php include "common.php"?>
+    <link rel="stylesheet" href="css/custom-bs.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="fonts/line-icons/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+      <link rel="stylesheet" href="css/flaticon.css">
+<style>
+.pb-3{animation-name: fadeInUp;animation-duration: 2s;}
+.collapse{visibility: visible !important;}
+	@media only screen and (max-width: 521px)
+{
+  #h_wirmon
+  {
+
+    margin-top: 2px !important;
+    height: 50px !important;
+    width: 150px !important;
+  }
+
+}
+@media only screen and (max-width: 767px)
+{
+  #h_wirmon
+  {
+    margin-top: 2px !important;
+    height: 50px !important;
+    width: 150px !important;
+  }
+}
+
+#img{
+
+   margin-left:50px;
+   font-family:sans-serif;
+   fon-syle:bold;
+
+   border:50px;
+}
+#top{
+    background-color:#4b79a1;
+}
+
+
+</style>
+    <!-- MAIN CSS -->
+    <script src="js/main.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/wow.js"></script>
+
+  </head>
+  <body id="top" data-aos-easing="slide" data-aos-duration="800" data-aos-delay="0">
+  <!-- NAVBAR -->
+    <div class="site-wrap">
+
+        <div class="site-mobile-menu site-navbar-target">
+          <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+              <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+          </div>
+          <div class="site-mobile-menu-body"></div>
+        </div> <!-- .site-mobile-menu -->
+
+
+        <!-- NAVBAR -->
+    <div class="site-logo col-6"><a href="index.php"><img src="images/logo.png" id="h_wirmon" style="height:70px;width: 200px;margin-top:20px;margin-left:30px;"></a></div>
+    
+    <!-- HOME -->
+   
+     <section style="padding: 4em 0;">
+      <div class="container">
+        <div class="row d-flex">
+          <div class="col-md-3 d-flex align-self-stretch">
+            <div class="media block-6 services d-block" id="img">
+              <div class="icon fadeInUp wow" style="animation-duration:2s;visibility:visible !important;"><a href="link.php"><img src="images/admin.webp"></a></div>
+              <div class="media-body">
+                <h3>Admin Login</h3></p>
+              </div>
+             </div>
+           </div>
+           <div class="col-md-3 d-flex align-self-stretch">
+            <div class="media block-6 services d-block" id="img">
+              <div class="icon fadeInUp wow" style="animation-duration:2s;visibility:visible !important;"><a href="link.php"><img src="images/HRMS.png"></a></div>
+              <div class="media-body">
+                <h3>HRMS</h3></p>
+              </div>
+             </div>
+           </div>
+            <div class="col-md-3 d-flex align-self-stretch">
+            <div class="media block-6 services d-block" id="img">
+              <div class="icon fadeInUp wow" style="animation-duration:2s;visibility:visible !important;"><a href="link.php"><img src="images/wemail_login.webp"></a></div>
+              <div class="media-body">
+                <h3>Webmail Login</h3></p>
+              </div>
+             </div>
+           </div>
+           <div class="col-md-3 d-flex align-self-stretch">
+            <div class="media block-6 services d-block" id="img">
+              <div class="icon fadeInUp wow" style="animation-duration:2s;visibility:visible !important;"><a href="link.php"><img src="images/attendance.webp"></a></div>
+              <div class="media-body">
+                <h3>Attendance Login</h3></p>
+              </div>
+             </div>
+           </div>
+            <div class="col-md-3 d-flex align-self-stretch">
+            <div class="media block-6 services d-block" id="img">
+              <div class="icon fadeInUp wow" style="animation-duration:2s;visibility:visible !important;"><a href="link.php"><img src="images/workplace.webp"></a></div>
+              <div class="media-body">
+                <h3>Workplace Login</h3></p>
+              </div>
+             </div>
+           </div>
+          
+        
+        
+        
+        </div>
+      </div>
+    </section>
+    
+    
+
+
+                  
