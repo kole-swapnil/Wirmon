317450a190a90e0e658f8ea348290538b70cf656
Swapnil Kole <swapnilkole7500@gmail.com>
2020-09-16 00:51:54 +0530

    faq working on

--- faq.php
+++ faq.php
@@ -127,47 +127,180 @@ session_destroy();
           <div class="col-lg-5 ml-auto">
             <div class="accordion-item">
               <h3 class="mb-0 heading">
-                <a class="btn-block h4" data-toggle="collapse" href="#collapseFive" role="button" aria-expanded="true" aria-controls="collapseFive">What is the name of your company<span class="icon"></span></a>
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseFive" role="button" aria-expanded="true" aria-controls="collapseFive">What is Onsite Recruitment?<span class="icon"></span></a>
               </h3>
               <div id="collapseFive" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
                 <div class="body-text">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur quae cumque perspiciatis aperiam accusantium facilis provident aspernatur nisi optio debitis dolorum, est eum eligendi vero aut ad necessitatibus nulla sit labore doloremque magnam! Ex molestiae, dolor tempora, ad fuga minima enim mollitia consequuntur, necessitatibus praesentium eligendi officia recusandae culpa tempore eaque quasi ullam magnam modi quidem in amet. Quod debitis error placeat, tempore quasi aliquid eaque vel facilis culpa voluptate.</p>
+                  <p>Onsite recruitment is a service where a recruitment agency effectively becomes part of an organisation, taking care of all aspects of recruitment for that business. It means that services can be tailored exactly to the needs of the business and the workforce, establishing an efficient working partnership. The recruitment agency effectively becomes an extension of the workforce, working to improve the hiring process, reducing risk and cost per hire whilst aiding retention and staff management.</p>
                 </div>
               </div>
             </div> <!-- .accordion-item -->
 
             <div class="accordion-item">
               <h3 class="mb-0 heading">
-                <a class="btn-block h4" data-toggle="collapse" href="#collapseSix" role="button" aria-expanded="false" aria-controls="collapseSix">How much pay for 3  months?<span class="icon"></span></a>
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseSix" role="button" aria-expanded="false" aria-controls="collapseSix"> What is payroll Service and advantages of it?<span class="icon"></span></a>
               </h3>
               <div id="collapseSix" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                 <div class="body-text">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad laborum expedita. Nostrum iure atque enim quisquam minima distinctio omnis, consequatur aliquam suscipit, quidem, esse aspernatur! Libero, excepturi animi repellendus porro impedit nihil in doloremque a quaerat enim voluptatum, perspiciatis, quas dignissimos maxime ut cum reiciendis eius dolorum voluptatem aliquam!</p>
+                  <p>Payroll is a list of employees who get paid by the company. Payroll also refers to the total
+amount of money employer pays to the employees. As a business function, it involves:
+• Developing organization pay policy including flexible benefits, leave encashment
+policy, etc.
+• Defining payslip components like basic, variable pay, HRA, and LTA
+• Gathering other payroll inputs (e.g., organization’s food vendor may supply
+information about the amount to be recovered from the employees for meals
+consumed)
+• The actual calculation of gross salary, statutory as well as non-statutory deductions,
+and arriving at the net pay
+• Releasing employee salary
+• Depositing dues like TDS, PF, etc. with appropriate authorities and filing returns
+</p>
                 </div>
               </div>
             </div> <!-- .accordion-item -->
 
             <div class="accordion-item">
               <h3 class="mb-0 heading">
-                <a class="btn-block h4" data-toggle="collapse" href="#collapseSeven" role="button" aria-expanded="false" aria-controls="collapseSeven">Do I need to register?  <span class="icon"></span></a>
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseSeven" role="button" aria-expanded="false" aria-controls="collapseSeven"> What is On boarding Services?  <span class="icon"></span></a>
               </h3>
               <div id="collapseSeven" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                 <div class="body-text">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad laborum expedita. Nostrum iure atque enim quisquam minima distinctio omnis, consequatur aliquam suscipit, quidem, esse aspernatur! Libero, excepturi animi repellendus porro impedit nihil in doloremque a quaerat enim voluptatum, perspiciatis, quas dignissimos maxime ut cum reiciendis eius dolorum voluptatem aliquam!</p>
+                  <p>When a client has a long-term need, direct hire is ideal. Direct hire positions are permanent,
+usually full-time positions with benefits. A staffing agency is involved during the recruitment
+and hiring process, but after an offer is accepted, the candidate goes directly on the client’s
+payroll.
+The process for direct hire can be a little slower because clients want to take their time when
+making a long-term decision. It is ideal for candidates who are not as comfortable taking the
+risk of working contract. Direct hire employees are eligible for company benefits such as
+health, retirement, and PTO, and have the security of a long-term, permanent position.</p>
                 </div>
               </div>
             </div> <!-- .accordion-item -->
 
             <div class="accordion-item">
               <h3 class="mb-0 heading">
-                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseEight">Who should I contact in case of support.<span class="icon"></span></a>
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseEight"> What are the staffing services provided by WIRMON?<span class="icon"></span></a>
               </h3>
               <div id="collapseEight" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                 <div class="body-text">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad laborum expedita. Nostrum iure atque enim quisquam minima distinctio omnis, consequatur aliquam suscipit, quidem, esse aspernatur! Libero, excepturi animi repellendus porro impedit nihil in doloremque a quaerat enim voluptatum, perspiciatis, quas dignissimos maxime ut cum reiciendis eius dolorum voluptatem aliquam!</p>
+                  <p>Hiring the right candidate is one of the biggest challenge of a staffing agency. If you are
+looking to recruit skilled employees for your organization, contact Wirmon HR today. We
+can help you with contractual as well as permanent staffing services. Wirmon’s expertise in
+providing staffing solutions enables you to focus your energies on your core business
+activities and spend less time on filling the contractual roles and administering the HR
+processes for the contract employees.
+</p>
                 </div>
               </div>
             </div> <!-- .accordion-item -->
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseNine"> What Is Talent Acquisition?<span class="icon"></span></a>
+              </h3>
+              <div id="collapseNine" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>Talent acquisition refers to the process of identifying and acquiring skilled workers to meet
+your organizational needs. The talent acquisition team is responsible for identifying,
+acquiring, assessing, and hiring candidates to fill open positions within a company. Employer
+branding, future resource planning, diversifying a company’s labour force, and developing a
+robust candidate pipeline are the cornerstones of talent acquisition.
+In some cases, the talent acquisition team is part of an organization’s Human Resources
+department. In others, Talent Acquisition is its own department that works in coordination
+with HR. The skill sets of effective talent acquisition professionals include sourcing
+strategies, candidate assessment, compliance and hiring standards, and fluency in
+employment branding practices and corporate hiring initiatives.
+</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseTen"> What if I want to changes to be made in the app once it is launched or delivered?<span class="icon"></span></a>
+              </h3>
+              <div id="collapseTen" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>If you want the changes to be made in the app,It will cost you. We recommend having the
+project scope defined before beginning the project to save time and money.
+</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseEleven">  What is your project development methodology?<span class="icon"></span></a>
+              </h3>
+              <div id="collapseEleven" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>Every web or mobile app development project is unique and requires a fresh approach, but
+most projects pass through some of the most command project development steps-i.e.
+Requirement gathering, Some of the most command project development steps-i.e. Requirement gathering, Analysis, Solution consulting, wireframe/prototyping,
+UI design, development and testing .However, depending on the scope and type of the
+software development project, the sequence and
+Selection of steps may vary.
+</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseTwelve"> Do you work according to client's Time zone?<span class="icon"></span></a>
+              </h3>
+              <div id="collapseTwelve" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>All of our development team works out of our Headquarter-Bangalore, India. However,
+depending on the nature of the work,
+Specific situation and in case of special meetings, we can certainly stay awake to ensure we
+connect during your working hours,
+However, we’d prefer having this be scheduled prior to the event so that we can plan our rest
+of the schedules accordingly.
+
+</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseThirteen"> What are the advantages of digital marketing services?
+<span class="icon"></span></a>
+              </h3>
+              <div id="collapseThirteen" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>Digital marketing services help you increase brand awareness, brand loyalty and drive sales
+for your business.
+The main advantage of Digital Marketing is that it helps you measure success, performance
+and results of all the activities across all platforms in a very cost-effective way
+
+</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseFourteen">  Do you provide placement assistance?<span class="icon"></span></a>
+              </h3>
+              <div id="collapseFourteen" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>We provide placement assistance opportunity after the completion of internship duration of
+intern it will depend on the intern performance how the intern has performed. They could get
+placement assistance for their future job.
+
+</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h3 class="mb-0 heading">
+                <a class="btn-block h4" data-toggle="collapse" href="#collapseEight" role="button" aria-expanded="false" aria-controls="collapseFifteen"> Which Framework or programming languages would you use to develop a mobile
+app?<span class="icon"></span></a>
+              </h3>
+              <div id="collapseEight" class="collapseFifteen" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="body-text">
+                  <p>The framework or programming language that we use to develop applications depend upon
+the chosen platform by the client.
+</p>
+                </div>
+              </div>
+            </div>
 
           </div>
 
