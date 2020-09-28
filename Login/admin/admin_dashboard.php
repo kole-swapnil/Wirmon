915f7273ab5dc3c79a3010219cda45c0ba196a09
soundarya-karli <soundaryakarli@gmail.com>
2020-09-28 12:08:04 +0530

    link working jobseeker.php

--- Login/admin/admin_dashboard.php
+++ Login/admin/admin_dashboard.php
@@ -63,7 +63,7 @@ include '../../dbconn.php';
 
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="jobseeker.php" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo" >
+        <a class="nav-link collapsed" href="jobseeker.php">
           <i class="fas fa-fw fa-cog"></i>
           <span>Jobseeker</span>
         </a>
@@ -78,7 +78,7 @@ include '../../dbconn.php';
 
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+        <a class="nav-link collapsed" href="#" >
           <i class="fas fa-fw fa-wrench"></i>
           <span>Employer</span>
         </a>
