a75f01655362595042eddff7b6ec619bdec2600e
Swapnil Kole <swapnilkole7500@gmail.com>
2020-09-28 21:14:17 +0530

    error

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ if($stmt97->rowCount() > 0)
     <meta charset="utf-8">
 
     <?php include "common.php"?>
-    <link rel="stylesheet" href="css/custom-bs.css">
+
     <link rel="stylesheet" href="css/jquery.fancybox.min.css">
     <link rel="stylesheet" href="css/bootstrap-select.min.css">
     <link rel="stylesheet" href="fonts/icomoon/style.css">
@@ -160,8 +160,8 @@ if($stmt97->rowCount() > 0)
                   <li><a href="index.php" class="active">Home</a></li>
                   <li><a href="about.php">About</a></li>
                   <li><a href="job-listings.php">Jobs</a> </li>
-                  <li class="has-children">
-                    <a>Services</a>
+                  <li class="has-children"><a>Services</a>
+                   
                     <ul class="dropdown">
                       <li><a href="services.php">Services</a></li>
                       <li><a href="portfolio.php">Portfolio</a></li>
