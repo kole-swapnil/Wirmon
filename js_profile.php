d292fd48c0ac2ed2701d80037bfaae31a33129b9
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-08 00:57:11 +0530

    Update js_profile.php

--- js_profile.php
+++ js_profile.php
@@ -90,7 +90,7 @@ else{
   <head>
     <title>Wirmon &mdash; Dashboard-Profile</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
   <?php include "common.php"?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/custom-bs.css">
