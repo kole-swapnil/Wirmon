9b97bb6d48e339828dab3fbd939718ea6ae6553f
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-08 00:51:52 +0530

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -35,7 +35,6 @@ if (isset($_POST['submit']))
   <head>
     <title>Contact Wirmon</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <?php include "common.php"?>
     <link rel="stylesheet" href="css/custom-bs.css">
