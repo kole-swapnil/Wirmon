5f3a01b08c958d4c1db710f3ab316916823bf3dc
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-08 00:52:16 +0530

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -4,7 +4,7 @@
   <head>
     <title>Wirmon &mdash; Gallery</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
 
 
     <link rel="stylesheet" href="css/custom-bs.css">
