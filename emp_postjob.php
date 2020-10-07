f86f40aabb125faa1d23727e63fea319be813150
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-08 00:50:28 +0530

    Update emp_postjob.php

--- emp_postjob.php
+++ emp_postjob.php
@@ -83,7 +83,7 @@ $filename  = 'skills.txt';
   <head>
     <title>Wirmon &mdash; Post Job</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <?php include "common.php"?>
 
     <link rel="stylesheet" href="css/custom-bs.css">
