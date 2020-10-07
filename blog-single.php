b6cbde9b58cf191fd2995eaae489bac59fad62bf
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-08 00:46:25 +0530

    Update blog-single.php

--- blog-single.php
+++ blog-single.php
@@ -4,7 +4,7 @@
   <head>
     <title>Wirmon &mdash; Blog</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php include "common.php"?>
 
     <link rel="stylesheet" href="css/custom-bs.css">
     <link rel="stylesheet" href="css/jquery.fancybox.min.css">
