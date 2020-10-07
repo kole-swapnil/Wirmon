fe0b89e0a4e98b8a51cc53e0b80b68261d7b3d19
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-07 23:54:27 +0530

    Update services.php

--- services.php
+++ services.php
@@ -10,7 +10,7 @@ session_destroy();
   <head>
     <title>Services from Wirmon</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <?php include "common.php"?>
 
     <link rel="stylesheet" href="css/custom-bs.css">
