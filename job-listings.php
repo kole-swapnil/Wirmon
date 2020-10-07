1abdc5c5cc94c442af3c4093bebebf481fdc374f
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-08 00:54:06 +0530

    Update job-listings.php

--- job-listings.php
+++ job-listings.php
@@ -26,7 +26,7 @@ $total_pages = ceil($total_rows / $no_of_records_per_page);
   <head>
     <title>Jobs in India | Jobs in Covid19</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <?php include "common.php"?>
 
     <link rel="stylesheet" href="css/custom-bs.css">
