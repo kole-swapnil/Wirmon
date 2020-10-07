08eeb0d9112739235db0e260f9d2e83be6181f2e
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-08 00:49:24 +0530

    Update digital_marketing.php

--- digital_marketing.php
+++ digital_marketing.php
@@ -84,8 +84,7 @@ if(isset($_POST['submit']))
   <head>
     <title>Wirmon &mdash; IT Services</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- <meta name="viewport" content="width=device-width, initial-scale=1">
+
    <?php include "common.php"?>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
