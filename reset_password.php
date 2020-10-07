cf76bd394cfec1ed661f72722a9a66e5fa302f34
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-07 23:58:21 +0530

    Update reset_password.php

--- reset_password.php
+++ reset_password.php
@@ -31,7 +31,7 @@ if (isset($_POST['submit']))
   <head>
     <title>Contact Wirmon</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   
 
   <?php include "common.php"?>
     <link rel="stylesheet" href="css/custom-bs.css">
