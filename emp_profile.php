045ff0eef9b4364e1e7cdba4f7ca396d64cc7b1e
sarthakinaik <59866909+sarthakinaik@users.noreply.github.com>
2020-10-08 00:50:50 +0530

    Update emp_profile.php

--- emp_profile.php
+++ emp_profile.php
@@ -75,7 +75,7 @@ else{
   <head>
     <title>Wirmon &mdash; Dashboard-Profile</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <?php include "common.php"?>
 
     <link rel="stylesheet" href="css/custom-bs.css">
